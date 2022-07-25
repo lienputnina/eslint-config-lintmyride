e789a3c9049c98619fc2d8a94ed2be55b801edda
LieneLittlebird <liene.putnina20@gmail.com>
2022-07-25 11:51:40 +0300

    eslint: turned off the import extension rule

--- package/src/base.js
+++ package/src/base.js
@@ -2,6 +2,7 @@ module.exports = {
   'no-console': 1,
   'linebreak-style': 0,
   'import/no-duplicates': 2,
+  'import/extensions': 0,
   eqeqeq: 2,
   '@typescript-eslint/no-var-requires': 0,
   '@typescript-eslint/no-non-null-assertion': 2,
